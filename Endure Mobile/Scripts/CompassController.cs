340b129cdab5c3c544343d6d15bcfe89cfc4b582
WeiJun Chen <peterchen096@gmail.com>
2020-03-02 22:19:54 -0800

    Compass

---
+++ Endure Mobile/Scripts/CompassController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassController : MonoBehaviour
+{
+	// Follows game-object in runtime using 2D Canvas 
+	
+    public GameObject pointer;
+    public GameObject target;
+    public GameObject player;
+    public RectTransform compassLine;
+    RectTransform rect;
+
+    void Start()
+    {
+        rect = pointer.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+
+        Vector3[] vector_3 = new Vector3[4];
+        compassLine.GetLocalCorners(v);
+        float pointerScale = Vector3.Distance(vector_3[1], vector_3[2]); //both bottom corners
+
+        Vector3 direction = target.transform.position - player.transform.position;
+        float angleToTarget = Vector3.SignedAngle(player.transform.forward, direction, player.transform.up);
+		
+        angleToTarget = Mathf.Clamp(angleToTarget, -90, 90) / 180.0f * pointerScale;
+        rect.localPosition = new Vector3(angleToTarget, rect.localPosition.y, rect.localPosition.z);
+    }
+}
